f7cacfcd9d8243aa65c3f521b88b1341f561c952
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-01-21 02:01:16 +0100

    refactor: rename data var

--- match-api/prisma/Pokefumi-TwoPlayers-match.sql
+++ match-api/prisma/Pokefumi-TwoPlayers-match.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS MATCH CASCADE;
-DROP TABLE IF EXISTS MROUND CASCADE;
-DROP TYPE IF EXISTS match_status CASCADE;
-CREATE TYPE "match_status" AS ENUM ('pending', 'started', 'finished');
-CREATE TABLE "MATCH" (
+DROP TABLE IF EXISTS "Match" CASCADE;
+DROP TABLE IF EXISTS "Round" CASCADE;
+DROP TYPE IF EXISTS "MatchStatus" CASCADE;
+CREATE TYPE "MatchStatus" AS ENUM ('pending', 'started', 'finished');
+CREATE TABLE "Match" (
   "id" SERIAL PRIMARY KEY,
-  "status" match_status,
+  "status" "MatchStatus",
   "owner_id" integer,
   "opponent_id" integer,
   "owner_deck_id" integer,
   "opponent_deck_id" integer
 );
-CREATE TABLE "MROUND" (
+CREATE TABLE "Round" (
   "id" SERIAL PRIMARY KEY,
   "match_id" integer,
   "owner_poke_id" integer,
@@ -18,5 +18,5 @@ CREATE TABLE "MROUND" (
   "winner_id" integer,
   "turn" int
 );
-ALTER TABLE "MROUND"
-ADD FOREIGN KEY ("match_id") REFERENCES "MATCH" ("id");
+ALTER TABLE "Round"
+ADD FOREIGN KEY ("match_id") REFERENCES "Match" ("id");
